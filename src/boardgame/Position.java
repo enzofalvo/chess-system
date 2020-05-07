44072fb892e1705277491bca6e89f4a474d74fa8
Enzo Falvo <bestwordno@gmail.com>
2020-05-07 12:09:47 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardgame;
+
+/**
+ *
+ * @author Enzo
+ */
+public class Position {
+    private int row;
+    private int column;
+    
+    public Position() {        
+    }
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+    
+}
